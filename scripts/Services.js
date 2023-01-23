985c9ab88f417c2d9de6408b023f60680ee7076f
Jordan Victoria <jgomeskuehner@gmail.com>
2023-01-23 14:20:12 -0600

    MVP

--- scripts/Services.js
+++ scripts/Services.js
@@ -4,6 +4,33 @@ const services = getServices()
 const parkAreas = getParkAreas()
 const serviceAreas = getParkAreaServices()
 
+
+
+
+
+export const Services = () => {
+    let servicesHTML = "<div class='services'>"
+
+    const listItems = services.map(service => {
+        return `<h4 id="serviceList--${service.id}"> ${service.type}</h4>`
+    })
+
+    servicesHTML += listItems.join("")
+    servicesHTML += "</div>"
+    return servicesHTML
+}
+
+
+
+
+
+
+
+
+
+
+
+
 // helper function matching service objects to the serviceAreas array
 const findService = (services, serviceArea) => {
     let matchedService = ""
@@ -34,7 +61,7 @@ export const matchedServiceAreas = (parkArea) => {
         const park = findParkArea(parkAreas, serviceArea)
         const service = findService(services, serviceArea)
         if (park.id === parkArea.id)
-        html += `<li id="servicearea--${serviceArea.id}">${service.type}</li>`
+        html += `<li id="serviceArea--${serviceArea.id}">${service.type}</li>`
     }
     html += "</ul>"
 
@@ -49,3 +76,25 @@ Creating two helper functions:
 1. match the service to the serviceAreaId and return the service object
 2. match the parkArea to the parkAreaId and return the parkArea object
 */
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        let parkString = ""
+        if (itemClicked.id.startsWith ("serviceList")){
+            const [, serviceId] = itemClicked.id.split("--") 
+            for (const serviceArea of serviceAreas){
+                const park = findParkArea(parkAreas, serviceArea)
+                if (serviceArea.serviceId === parseInt(serviceId)){
+                    parkString += `\n${park.section}`
+
+                }
+                
+            }
+            window.alert(`This service is available in ${parkString}`)
+        }
+        
+    }
+)
